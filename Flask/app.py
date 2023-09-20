e772ec220dbf018dfcccebd7ae290aa38b894985
Sarim Zahid <106787132+Mikkk1@users.noreply.github.com>
2023-09-20 19:43:05 +0500

    Updates

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+
+# pip install redirect
 # WSGI Application
 app = Flask(__name__)
 
@@ -7,6 +9,22 @@ app = Flask(__name__)
 def hello():
     return "Hello World! This is m first Flask app"
 
+@app.route('/success/<int:score>')
+def success(score):
+    return "You got " + str(score)
+
+@app.route('/success/<int:score>')
+def fail(score):
+    return "You failed. You got " + str(score)
+
+@app.route('/results/<int:score>')
+def result(score):
+    if score > 40: 
+        result =  'success'
+    else
+        result = 'fail'
+    # return result
+    return redirect(url_for(result, score= score))
 if __name__ == "__main__":
-    app.run()
-    # app.run(debug = True)
+    # app.run()
+    app.run(debug = True)
